b9ec54042fccc980a85b7e36696542c5a2b0b688
Raphael Stefanini <raphael@rphlo.com>
2026-04-21 07:17:17 +0300

    Fix crash

--- routechoices/lib/geojson.py
+++ routechoices/lib/geojson.py
@@ -1,16 +1,16 @@
 def get_geojson_coordinates(gj):
-    if gj["type"] == "Point":
+    if gj["type"] == "Point" and gj["coordinates"]:
         return gj["coordinates"]
-    elif gj["type"] in ("LineString", "MultiPoint"):
+    elif gj["type"] in ("LineString", "MultiPoint") and gj["coordinates"]:
         return gj["coordinates"][0]
-    elif gj["type"] in ("Polygon", "MultiLineString"):
+    elif gj["type"] in ("Polygon", "MultiLineString") and gj["coordinates"]:
         return gj["coordinates"][0][0]
-    elif gj["type"] == "MultiPolygon":
+    elif gj["type"] == "MultiPolygon" and gj["coordinates"]:
         return gj["coordinates"][0][0][0]
-    elif gj["type"] == "Feature":
+    elif gj["type"] == "Feature" and gj["geometry"]:
         return get_geojson_coordinates(gj["geometry"])
-    elif gj["type"] == "GeometryCollection":
+    elif gj["type"] == "GeometryCollection" and gj["geometries"]:
         return get_geojson_coordinates(gj["geometries"][0])
-    elif gj["type"] == "FeatureCollection":
+    elif gj["type"] == "FeatureCollection" and gj["features"]:
         return get_geojson_coordinates(gj["features"][0])
     return None
