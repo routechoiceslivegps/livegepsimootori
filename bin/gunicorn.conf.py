919d154fcf95b1a02ae6feebd15781f176680e27
Raphael Stefanini <raphael@rphlo.com>
2026-04-30 20:59:19 +0300

    gevent

--- bin/gunicorn.conf.py
+++ bin/gunicorn.conf.py
@@ -2,7 +2,7 @@ wsgi_app = "routechoices.wsgi:application"
 preload_app = True
 daemon = False
 raw_env = ["DJANGO_SETTINGS_MODULE=routechoices.settings"]
-workers = 4
-threads = 2
+workers = 5
 max_requests = 3000
 max_requests_jitter = 100
+worker_class = "gevent"
