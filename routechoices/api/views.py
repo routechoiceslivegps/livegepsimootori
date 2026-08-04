b16eab140e0a10219221ca475e654969687a3b10
Raphael Stefanini <raphael@rphlo.com>
2026-08-04 08:38:34 +0300

    Add server name

--- routechoices/api/views.py
+++ routechoices/api/views.py
@@ -1338,6 +1338,7 @@ def event_data_delta(request, event_id, previous_key):
     req.method = "GET"
     req.user = request.user
     req.session = request.session
+    req.META["SERVER_NAME"] = ""
     if tag:
         req.GET.update({"category": tag})
     current_resp = event_data(req, event_id)
